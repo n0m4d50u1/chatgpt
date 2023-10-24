c8852dfea6ef4b3cffe5adf62778330e2ecdef81
n0m4d50u1 <boscher.alexis@icloud.com>
2023-10-24 18:29:18 +0200

    more functions added

--- app.py
+++ app.py
@@ -1,26 +1,74 @@
-import openai
 import os
+import openai
 
-# Set your OpenAI API key
+# Remplacez 'OPENAI_API_KEY' par votre clé d'API OpenAI
 openai.api_key = os.getenv('OPENAI_API_KEY')
-#openai.api_key = 'YOUR_OPENAI_API_KEY'
 
-# Function to interact with ChatGPT
-def chat_with_gpt(prompt):
+# Fonction pour interagir avec ChatGPT et obtenir plusieurs suggestions de complétion
+def chat_with_gpt(prompt, num_completions=3, max_tokens=150):
     response = openai.Completion.create(
-        engine="text-davinci-003",  # You can also use "text-davinci-002" for older version
+        engine="text-davinci-003",
         prompt=prompt,
-        max_tokens=150  # Adjust the response length as per your requirement
+        max_tokens=max_tokens,
+        n=num_completions,
+        stop=None
     )
-    return response.choices[0].text.strip()
+    completions = [choice['text'].strip() for choice in response.choices]
+    return completions
 
-# Main interaction loop
-print("ChatGPT: Hello! How can I assist you today? (Type 'exit' to end)")
+# Fonction pour poser une question à ChatGPT avec des suggestions de complétion
+def poser_question(question):
+    prompt = f"Question : {question}\nRéponse :"
+    completions = chat_with_gpt(prompt)
+    print("Suggestions de ChatGPT :")
+    for i, completion in enumerate(completions, start=1):
+        print(f"{i}. {completion}")
+    choix = int(input("Choisissez la réponse (1, 2, 3, ...) ou tapez 0 pour poser une autre question: "))
+    if choix > 0 and choix <= len(completions):
+        return completions[choix - 1]
+    else:
+        return None
+
+# Fonction pour lire et analyser un fichier
+def lire_fichier(nom_fichier):
+    try:
+        with open(nom_fichier, 'r', encoding='utf-8') as file:
+            contenu = file.read()
+        return contenu
+    except FileNotFoundError:
+        print("Fichier non trouvé.")
+        return None
+    except Exception as e:
+        print(f"Erreur lors de la lecture du fichier : {str(e)}")
+        return None
+
+# Exemple d'interaction avec ChatGPT, suggestions de complétion et lecture de fichier
 while True:
-    user_input = input("You: ")
-    if user_input.lower() == 'exit':
-        print("ChatGPT: Goodbye!")
+    print("1. Poser une question")
+    print("2. Lire un fichier")
+    print("3. Quitter")
+    choix = input("Choisissez une option (1, 2, 3) : ")
+
+    if choix == "1":
+        utilisateur_input = input("Vous: ")
+        reponse = poser_question(utilisateur_input)
+        if reponse:
+            print("ChatGPT:", reponse)
+        else:
+            print("ChatGPT: Je ne comprends pas. Veuillez reformuler votre question.")
+
+    elif choix == "2":
+        nom_fichier = input("Entrez le nom du fichier à lire : ")
+        contenu_fichier = lire_fichier(nom_fichier)
+        if contenu_fichier:
+            reponse = chat_with_gpt(contenu_fichier, num_completions=1, max_tokens=200)[0]
+            print("Analyse du fichier par ChatGPT :", reponse)
+        else:
+            print("Impossible de lire le fichier.")
+
+    elif choix == "3":
+        print("Au revoir!")
         break
-    prompt = f"You: {user_input}\nChatGPT:"
-    response = chat_with_gpt(prompt)
-    print(response)
+
+    else:
+        print("Option invalide. Veuillez choisir une option valide (1, 2, 3).")
